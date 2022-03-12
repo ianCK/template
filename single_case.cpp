79727fc9cf205d0ec3254c91b8965828e3754494
ianCK <b07102015@ntu.edu.tw>
2022-03-13 03:09:01 +0800

    Update single_case.cpp

--- single_case.cpp
+++ single_case.cpp
@@ -1,31 +1,4 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define PB push_back
-#define F first
-#define S second
-#define MP make_pair
-#define MTP make_tuple
-#define R Read
-#define RD Read_Digit
-#define RP Read_P
-#define RS Read_String
-#define RL Read_Loop
-#define RLD Read_Loop_Digit
-#define RLP Read_Loop_P
-#define RLS Read_Loop_String
-#ifdef ONLINE_JUDGE
-	#define Debug(...) ;
-	#define Debug_Array(n,x) ;
-	#define Debugln_Array(n,x) ;
-	#define NL ;
-#else
-	#define Debug(...) {printf("(%s) = ",(#__VA_ARGS__)),_print(__VA_ARGS__),printf("\n");}
-	#define Debug_Array(n,x) {printf("%s :",(#x));for(int i=1;i<=n;i++)printf(" "),_print(x[i]);printf("\n");}
-	#define Debugln_Array(n,x) {for(int i=1;i<=n;i++){printf("%s",(#x));printf("[%d] = ", i);_print(x[i]);printf("\n");}}
-	#define NL {printf("\n");}
-#endif
-typedef long long int ll;
-typedef unsigned long long int ull;
+#include "C:\Users\ianli\Desktop\CP\template\CommonInclude\CommonInclude.h"
 
 constexpr int kN = int(1E5 + 10);
 // constexpr int kMod = 998244353;
