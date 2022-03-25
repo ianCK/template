34212733ea31308e41b2bf5a26a039b5051872f1
ianCK <b07102015@ntu.edu.tw>
2022-03-25 19:28:44 +0800

    Installed minifier

--- Applications/Minifier/export.cpp
+++ Applications/Minifier/export.cpp
@@ -1,5 +1,5 @@
 // Merged by FileMerger.exe
-// --- Start of C:\Users\ianli\Desktop\CP\template\Various\Debug\test2.cpp --- 
+// --- Start of C:\Users\ianli\Desktop\CP\template\Applications\Minifier\test.cpp --- 
 // --- Start of C:\Users\ianli\Desktop\CP\template\CommonInclude\CommonInclude.h --- 
 
 #include <bits/stdc++.h>
@@ -1009,57 +1009,8 @@ template <int... Is, typename... Targs> void Debugger::print(const tuple<Targs..
 
 // using Mint = Mod_Int<kMod>;
 
-int abc[] = {0, 1, 2, 3, 3, 4, 2, 1};
-
 int main() {
 	FastIO IO;
 
-	int a = 3;
-	bool b = false;
-	char c = 'A';
-	double d = 0.3;
-	float f = 0.2;
-	long double lf = 0.14;
-
-	bitset<10> bs;
-	bs[2] = bs[5] = bs[9] = true;
-
-	vector<int> vec = {1, 3, 5};
-	vector<pair<double, char>> vec2 = {MP(0.3, 'A'), MP(1.32, 'B'), MP(1.45, 'Z')};
-
-	vector<float> vecemp;
-
-	stack<ll> st;
-	st.push(3);
-	st.push(1);
-	st.push(2);
-
-	queue<unsigned short> qu;
-	qu.push(3);
-	qu.push(1);
-	qu.push(2);
-
-	set<string> se;
-	se.insert("abc");
-	se.insert("cdf");
-	se.insert("qqq");
-
-	map<ull, bool> ma;
-	ma[3] = false;
-	ma[4] = true;
-	ma[12345] = false;
-
-	Debug(a, b, c, d, f, lf);
-	Debug(MTP(a, c));
-	Debug(MTP(a, c, d, lf));
-	Debug(bs, vec, vec2);
-	Debug(vecemp);
-	Debug("Good!");
-
-	Debug(st, qu);
-	Debug(se, ma);
-
-	DebugArray(7, abc);
-	DebugArray0Ln(8, abc);
 }
-// --- End of C:\Users\ianli\Desktop\CP\template\Various\Debug\test2.cpp --- 
+// --- End of C:\Users\ianli\Desktop\CP\template\Applications\Minifier\test.cpp --- 
