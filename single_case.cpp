6d5258082d034974b146ba973762fffcd506bcd8
ianCK <b07102015@ntu.edu.tw>
2022-10-23 14:18:19 +0800

    Update single_case.cpp

--- single_case.cpp
+++ single_case.cpp
@@ -1,8 +1,15 @@
 #include <CommonInclude\CommonInclude.h>
 
-constexpr int kN = int(1E5 + 10);
-// constexpr int kMod = 998244353;
-// constexpr int kMod = int(1E9 + 7);
+#include <Various\FastIO\FastIO.h>
+#include <Various\UsefulFunctions\UsefulFunctions.h>
+
+#ifndef ONLINE_JUDGE
+#include <Various\Debug\Debug.h>
+#endif
+
+constexpr int kN = 100'000 + 10; 
+// constexpr int kMod = 998'244'353;
+// constexpr int kMod = 1'000'000'000 + 7;
 // constexpr int kInf = 0x3f3f3f3f;
 // constexpr ll kInf = 0x3f3f3f3f3f3f3f3f;
 // constexpr double kPi = acos(-1);
@@ -12,16 +19,10 @@ constexpr int kN = int(1E5 + 10);
 // constexpr int dx[8] = {0, 0, 1, -1, 1, -1, 1, -1};
 // constexpr int dy[8] = {1, -1, 1, -1, -1, 1, 0, 0};
 
-#include <Various\FastIO\FastIO.h>
-#include <Various\UsefulFunctions\UsefulFunctions.h>
-
-#ifndef ONLINE_JUDGE
-#include <Various\Debug\Debug.h>
-#endif
-
 // using Mint = ModInt<kMod>;
 FastIO IO;
 
-int main() {
+int main()
+{
 
 }
